0074a46902cc13161f04422b3cc46bd106affca7
MateWagner <wwagnermate@gmail.com>
2023-03-27 18:07:06 +0200

    feat: Good annotation, jdbcTemplate added one legit method implementation

---
+++ src/main/java/com/codecool/stackoverflowtw/queston/QuestionsDaoJdbc.java
@@ -0,0 +1,25 @@
+package com.codecool.stackoverflowtw.queston;
+
+import com.codecool.stackoverflowtw.queston.QuestionsDAO;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class QuestionsDaoJdbc implements QuestionsDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    public QuestionsDaoJdbc(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Question> getAllQuestion() {
+        String sql = """
+                SELECT id, client_id, title, description, date
+                FROM question;
+                """;
+        return jdbcTemplate.query(sql, new QuestionRowMapper());
+    }
+}
