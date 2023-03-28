15a45ca6a04bfc5cc345573787af09a40672d4bf
MateWagner <wwagnermate@gmail.com>
2023-03-28 08:27:20 +0200

    feat: owner id added to answer

--- src/main/resources/dbInit.sql
+++ src/main/resources/dbInit.sql
@@ -1,25 +1,27 @@
-CREATE TABLE client
-(
-    id       SERIAL PRIMARY KEY,
-    name     text not null,
-    email    text not null,
-    password TEXT,
-    date     DATE
-);
-
-CREATE TABLE question (
-    id SERIAL PRIMARY KEY,
-    client_id int not null ,
-    title text not null ,
-    description text not null,
-    date DATE not null ,
-    FOREIGN KEY (client_id) REFERENCES client(id)
-);
+-- CREATE TABLE client
+-- (
+--     id       SERIAL PRIMARY KEY,
+--     name     text not null,
+--     email    text not null,
+--     password TEXT,
+--     date     DATE
+-- );
+--
+-- CREATE TABLE question (
+--     id SERIAL PRIMARY KEY,
+--     client_id int not null ,
+--     title text not null ,
+--     description text not null,
+--     date DATE not null ,
+--     FOREIGN KEY (client_id) REFERENCES client(id)
+-- );
 
 CREATE TABLE answer (
     id SERIAL PRIMARY KEY,
     description text not null ,
     date DATE not null ,
     question_id INT not null ,
-    answer_to_id int
+    answer_to_id int,
+    client_id int not null,
+    FOREIGN KEY (client_id) REFERENCES client(id)
 );
