65c218a0f97471dfa074d310df0d75e30c967ea9
MateWagner <wwagnermate@gmail.com>
2023-03-28 11:11:20 +0200

    feat: new method to add new question

--- src/main/java/com/codecool/stackoverflowtw/queston/QuestionsDAO.java
+++ src/main/java/com/codecool/stackoverflowtw/queston/QuestionsDAO.java
@@ -1,7 +1,10 @@
 package com.codecool.stackoverflowtw.queston;
 
+import com.codecool.stackoverflowtw.queston.dto.NewQuestionDTO;
+
 import java.util.List;
 
 public interface QuestionsDAO {
     List<Question> getAllQuestion();
+    Integer addNewQuestion(NewQuestionDTO newQuestionDTO);
 }
