ab5cdcfb3a06219db2358fba4605e25b55f917c6
MateWagner <wwagnermate@gmail.com>
2023-03-28 11:08:23 +0200

    feat: add time stamp to date

--- src/main/resources/dbInit.sql
+++ src/main/resources/dbInit.sql
@@ -1,20 +1,19 @@
--- CREATE TABLE client
--- (
---     id       SERIAL PRIMARY KEY,
---     name     text not null,
---     email    text not null,
---     password TEXT,
---     date     DATE
--- );
---
--- CREATE TABLE question (
---     id SERIAL PRIMARY KEY,
---     client_id int not null ,
---     title text not null ,
---     description text not null,
---     date DATE not null ,
---     FOREIGN KEY (client_id) REFERENCES client(id)
--- );
+CREATE TABLE client(
+    id       SERIAL PRIMARY KEY,
+    name     text not null,
+    email    text not null,
+    password TEXT,
+    date     DATE
+);
+
+CREATE TABLE question (
+    id SERIAL PRIMARY KEY,
+    client_id int not null ,
+    title text not null ,
+    description text not null,
+    date timestamp without time zone not null ,
+    FOREIGN KEY (client_id) REFERENCES client(id)
+);
 
 CREATE TABLE answer (
     id SERIAL PRIMARY KEY,
