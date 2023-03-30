0bf508624df76f14a09c8aea6fe3713f85234f16
MateWagner <wwagnermate@gmail.com>
2023-03-30 13:09:47 +0200

    feat: overloaded method to accept ordering parameters

--- src/main/java/com/codecool/stackoverflowtw/queston/QuestionsDAO.java
+++ src/main/java/com/codecool/stackoverflowtw/queston/QuestionsDAO.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 public interface QuestionsDAO {
     List<QuestionDTO> getAllQuestion();
+    List<QuestionDTO> getAllQuestion(String orderedBy, String order);
     Integer addNewQuestion(NewQuestionDTO newQuestionDTO);
     int deleteQuestionById(int id);
     Optional<QuestionDTO> getQuestionById(int id);
